a965da0dac56330b12a24d2e6d39f2dc15f38337
Beren Millidge <bmillidgework@gmail.com>
2017-10-12 01:15:58 +0100

    Add files via upload

---
+++ ajax_tutorial/index.php
@@ -0,0 +1,6 @@
+<?php header( 'Location: /index.html' );
+	# this is done so it will think of it as a dynamic site
+
+	#to reinstate the webservice, go to godaddy, manage domains, click the ellipsis, go to manage DNS, change the CNAME www, where it probably is @, to www.enyogroup.com.herokudns.com
+# and wait a bit for the change to propagate
+?>
